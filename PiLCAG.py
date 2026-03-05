1f0a1b368e690fda642ba5bee25dd8b1f13c03e0
Gordon999 <Gordon999@users.noreply.github.com>
2026-03-05 14:22:03 +0000

    v7.02 minor fix to scr cap

--- PiLCAG.py
+++ PiLCAG.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Copyright (c) 2025
+"""Copyright (c) 2026
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -35,8 +35,6 @@ getcontext().prec = 8
 version = 7.02
 
 #WORKS WITH PYTHON 3 
-
-# If you want to run at boot using Buster: Assuming you've saved it as /home/pi/PiLCAG.py
 
 #=============================================================================================================
 use_config  =         0 # set pre-stored config to load at startup, 1, 2 or 3. 
